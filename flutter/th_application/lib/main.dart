7a98975c83f2ece43f05603d443fe3b1e174565c
김지성 <jiseong2002@naver.com>
2025-11-19 22:05:06 +0900

    make base flutter

--- flutter/th_application/lib/main.dart
+++ flutter/th_application/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +20,62 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   // 문자열 선언
   String serverMessage = "Server Connecting...";
+  // 소켓 선언, ?로 null 여부 허용
+  Socket? channel;
+
+  // 소켓 연결 시도
+  // async로 시간이 걸리는 작업의 포함여부 표시
+  void _connectToServer() async{
+    try {
+      print("try connect server");
+
+      // await으로 비동기 연결
+      channel = await Socket.connect('192.168.0.4', 5000);
+
+      print("connect complete");
+
+      // 연결 성공시 화면 갱신
+      setState(() {
+        serverMessage = "connect complete ";
+      });
+
+      channel!.listen((Uint8List data){
+        String msg = utf8.decode(data);
+        print("listen server: $msg");
+
+        try {
+          // 서버로부터 받은 json 디코드
+          Map<String, dynamic> jsonData = jsonDecode(msg);
+
+          // json의 타입 확인 후 리스트로 나누기
+          if (jsonData['type'] == 'HOLE_CARDS'){
+            List<dynamic> cards = jsonData['cards'];
+
+            String suit1 = cards[0]['suit'];
+            int rank1 = cards[0]['rank'];
+
+            setState(() {
+              serverMessage = "recv card!\n $suit1 $rank1";
+            });
+          }
+        }
+        catch (e){
+          debugPrint("pass");
+        }
+      },);
+    }
+    catch(e){
+      print("connect filed: $e");
+    }
+  }
+
+  // 프로그램 종료시 종료
+  @override
+  void dispose(){
+    channel?.close();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +88,21 @@ class _MainAppState extends State<MainApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(serverMessage, style: const TextStyle(fontSize: 20)),
-              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  serverMessage,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(height: 30),
 
+              // 연결 시도 버튼
               ElevatedButton(
                 onPressed: () {
                   print("connect Try");
+                  _connectToServer();
                 },
                 child: const Text("server Connect"),
               ),
--- python/main.py
+++ python/main.py
@@ -1,36 +1,100 @@
 import socket
+import random
+import json
 
 HOST = '0.0.0.0'  
 PORT = 5000
 BUFFSIZE = 1024
 
+class Card:
+    def __init__(self, suit, rank):
+        # 무늬 초기화
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        # 만약 수가 문양이라면 문양으로 바꿔서 출력
+        displayRank = self.rank
+        if self.rank == 11:
+            displayRank = 'J'
+        elif self.rank == 12:
+            displayRank = 'Q'
+        elif self.rank == 13:
+            displayRank = 'K'
+        elif self.rank == 14:
+            displayRank = 'A'
+        return f"{self.suit} {displayRank}"
+    def to_dict(self):
+        # 딕셔너리로 변환해 반환
+        return {"suit":self.suit, "rank":self.rank}
+
+class Deck:
+    def __init__(self):
+        # 클래스 만들며 리스트 생성
+        self.cards = []
+        self.suitList = ["spade", "diamond", "heart", "clover"]
+        
+        self.make_deck()
+    
+    # 덱 생성
+    def make_deck(self):
+        # 리스트에 카드 만들어 삽입
+        # 리스트 초기화
+        # self.cards = []
+        for i in self.suitList:
+            for j in range(2, 15):
+                card = Card(i, j)
+                self.cards.append(card)
+        self.shuffle()
+
+    # 카드 섞기
+    def shuffle(self):
+        random.shuffle(self.cards)
+    
+    # 카드 뽑기
+    def draw(self):
+        if len(self.cards) > 0:
+            return self.cards.pop()
+        else:
+            self.make_deck()
+            return self.cards.pop()
+    
+    # 길이 반환
+    def __len__(self):
+        return len(self.cards)
+
 def start_server():
+    # 소켓 생성
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serverSocket.bind((HOST, PORT))
-
-    # ���� ���
+    # 접속 대기
     serverSocket.listen()
     print(f"[Server] {HOST}:{PORT} ready")
 
     try:
-        # ���� ����
+        # 접속 수락
         clientSocket, addr = serverSocket.accept()
         print(f"client accept {addr}")
 
-        # ������ ����
-        data = clientSocket.recv(BUFFSIZE)
-        print(f"RECV MSG {data.decode()}")
+        deck = Deck()
+        
+        card1 = deck.draw()
+        card2 = deck.draw()
 
-        # 7. ������ �۽�
-        msg = "ACK"
-        clientSocket.send(msg.encode())
-        print(f"SEND {msg}")
+        game_data = {
+            "type": "HOLE_CARDS",
+            "cards": [card1.to_dict(), card2.to_dict()]
+        }
+
+        # 카드 정보 json으로 변경 후 utf-8로 전송
+        json_str = json.dumps(game_data)
+        clientSocket.send(json_str.encode("utf-8"))
+
 
     except Exception as e:
         print(f"ERROR {e}")
     finally:
-        # ���� ����
+        # 소켓 정리
         clientSocket.close()
         serverSocket.close()
 
